c6cc7354eb6e7c2116c59130f51a7d40e77729cf
RuslanSkl <rsklyarenko@gmail.com>
2025-01-17 19:03:06 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+'use strict';
+let startX;
+let endX;
+const slider = document.querySelector('.slider');
+slider.addEventListener('touchstart', (e)=>{
+    startX = e.changedTouches[0].clientX;
+    console.log(startX);
+});
+slider.addEventListener('touchend', (e)=>{
+    endX = e.changedTouches[0].clientX;
+    handleSwipe();
+});
+function handleSwipe() {
+    if (startX - endX > 100) {
+        const nextSlide = document.querySelector('input[name="slide"]:checked').nextElementSibling;
+        if (nextSlide) nextSlide.checked = true;
+    } else if (startX - endX < -100) {
+        const prevSlide = document.querySelector('input[name="slide"]:checked').previousElementSibling;
+        if (prevSlide) prevSlide.checked = true;
+    }
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
